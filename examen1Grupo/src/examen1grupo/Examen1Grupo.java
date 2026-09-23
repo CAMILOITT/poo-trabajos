0b7705e72b6f3849980e17e83abda2536a66ee0b
CAMILOITT <camilocamilo5@outlook.com>
2026-09-23 09:37:51 -0500

    feature: Se creo la clase turismo

--- examen1Grupo/src/examen1grupo/Examen1Grupo.java
+++ examen1Grupo/src/examen1grupo/Examen1Grupo.java
@@ -14,7 +14,9 @@ public class Examen1Grupo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
+    public static  
+
 }
