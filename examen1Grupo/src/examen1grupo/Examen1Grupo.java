fb30a8a2f7fd1e1f5c6e8611768793b820d84ffa
CAMILOITT <camilocamilo5@outlook.com>
2026-09-23 10:26:53 -0500

    feature: Se a;adio la logica de las funciones no miembro de buscar sitio por ciudad y buscar sitio por rangos de costo

--- examen1Grupo/src/examen1grupo/Examen1Grupo.java
+++ examen1Grupo/src/examen1grupo/Examen1Grupo.java
@@ -13,27 +13,42 @@ import java.util.List;
  */
 public class Examen1Grupo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
 
-        List<SitioTuristico> listaSitios = new ArrayList<SitioTuristico>();
+		List<SitioTuristico> listaSitios = new ArrayList<SitioTuristico>();
 
-        // realizar el menu para el ingreso de datos
+		// realizar el menu para el ingreso de datos
 
-        // realizar el menu para escoger laopcion (debe de tener la opcion para salir
-        // del programa)
+		// realizar el menu para escoger la opcion (debe de tener la opcion para salir
+		// del programa)
 
-    }
+	}
 
-    public static SitioTuristico buscarSitiosTuristicosPorCiudad(List<SitioTuristico> sitio, String ciudad) {
-        return sitio;
-    }
+	public static List<SitioTuristico> buscarSitiosTuristicosPorCiudad(List<SitioTuristico> listaSitios, String ciudad) {
+		List<SitioTuristico> listaFiltrada = new ArrayList<SitioTuristico>();
 
-    public static List<SitioTuristico> buscarSitiosTurisiticosPorRangoDeCosto(List<SitioTuristico> sitio,
-            double costo) {
-        return sitio;
-    }
+		for (SitioTuristico sitioTuristico : listaSitios) {
+			if (sitioTuristico.getCiudad().equals(ciudad)) {
+				listaFiltrada.add(sitioTuristico)
+			}
+		}
+		return listaFiltrada;
+	}
+
+	public static List<SitioTuristico> buscarSitiosTurisiticosPorRangoDeCosto(List<SitioTuristico> listaSitios,
+			double costoInferior, double costoSuperior) {
+		List<SitioTuristico> listaFiltrada = new ArrayList<SitioTuristico>();
+
+		for (SitioTuristico sitioTuristico : listaSitios) {
+			if (costoInferior <= sitioTuristico.getCosto() && sitioTuristico.getCosto() <= costoSuperior) {
+				listaFiltrada.add(sitioTuristico);
+			}
+		}
+
+		return listaFiltrada;
+	}
 
 }
--- examen1Grupo/src/examen1grupo/SitioTuristico.java
+++ examen1Grupo/src/examen1grupo/SitioTuristico.java
@@ -9,32 +9,32 @@ package examen1grupo;
  * @author camil
  */
 public class SitioTuristico {
-    private String ciudad;
-    private String nombre;
-    private String descripcion;
-    private float costos;
+  private String ciudad;
+  private String nombre;
+  private String descripcion;
+  private float costos;
 
-    public SitioTuristico(String ciudad, String nombre, String descripcion, float costo) {
-        this.ciudad = ciudad;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.costos = costo;
-    }
+  public SitioTuristico(String ciudad, String nombre, String descripcion, float costo) {
+    this.ciudad = ciudad;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.costos = costo;
+  }
 
-    public String getCiudad() {
-        return this.ciudad;
-    }
+  public String getCiudad() {
+    return this.ciudad;
+  }
 
-    public String getNombre() {
-        return this.nombre;
-    }
+  public String getNombre() {
+    return this.nombre;
+  }
 
-    public String getDescripcion() {
-        return this.descripcion;
-    }
+  public String getDescripcion() {
+    return this.descripcion;
+  }
 
-    public Float getCosto() {
-        return this.costos;
-    }
+  public Float getCosto() {
+    return this.costos;
+  }
 
 }
